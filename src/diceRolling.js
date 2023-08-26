03678e80d7193936c7b1e4ce8880c5106483dacc
Alex H <alex.holder@bigfootds.com>
2023-08-26 11:00:07 +1000

    feat: rolling dice works, basic tests implemented

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -2,7 +2,12 @@
 
 function rollDice(diceSize = 6){
 
+	// Math.floor always rounds down, so diceSize of 6 would be 0-5
+    // Add 1 to guarantee that it stays within sensible range, 
+    // eg. diceSize of 6 means possible results are 1-6 
+	let diceTotal = Math.floor(Math.random() * diceSize) + 1;
 
+	return diceTotal;
 }
 
 
--- tests/diceRolling.test.js
+++ tests/diceRolling.test.js
@@ -1,13 +1,29 @@
 const {rollDice} = require('../src/diceRolling');
 
+beforeEach(() => {
+	console.log("message runs before each individual test()");
+});
+
+afterEach(() => {
+	console.log("message runs AFTER each individual test()");
+
+});
+
+
+
 describe("Casual tabletop gamer...", () => {
 
+	// xtest is the same as test.skip
 	// xtest("...wants to roll a D6 and see a valid result.", () => {
 
 	// });
 	
 	test("...wants to roll a D6 and see a valid result.", () => {
-		expect(rollDice()).toBeGreaterThan(0);
+
+		let diceResult = rollDice();
+
+		expect(diceResult).toBeGreaterThan(0);
+		expect(diceResult).toBeLessThanOrEqual(6);
 	});
 
 });
@@ -21,20 +37,59 @@ describe("Tabletop RPG gamer...", () => {
 
 	});
 
+	test("...wants to roll a D20 one hundred times and see how many natural 20s they get.", () => {
+		let diceResults = [];
+		for (let index = 0; index < 100; index++) {
+			diceResults.push(rollDice(20));
+		}
+
+		console.log(diceResults);
+
+		expect(diceResults).toContain(20);
+		// expect(diceResults.length).toEqual(100); // this line is the same as below:
+		expect(diceResults).toHaveLength(100);
+	});
+
 	// In D&D, we have these dice:
 	// D4, D6, D8, D10, D12, D20, D100
 
 	describe("...wants to roll a variety of dice. Specifically, they want to roll a...", () => {
-		test("D4", () => {
 
-		});
+		let diceVarieties = [
+			{
+				diceSize: 4, 
+				diceName: "D4"
+			},
+			{
+				diceSize: 6, 
+				diceName: "D6"
+			},
+			{
+				diceSize: 8, 
+				diceName: "D8"
+			},
+			{
+				diceSize: 10, 
+				diceName: "D10"
+			},
+			{
+				diceSize: 12, 
+				diceName: "D12"
+			},
+			{
+				diceSize: 20, 
+				diceName: "D20"
+			},
+			{
+				diceSize: 100, 
+				diceName: "D100"
+			},
+		]
 
-		test("D6", () => {
-
-		});
-
-		test("D8", () => {
-
+		test.each(diceVarieties)("$diceName", (diceObj) => {
+			let diceResult = rollDice(diceObj.diceSize);
+			expect(diceResult).toBeGreaterThan(0);
+			expect(diceResult).toBeLessThanOrEqual(diceObj.diceSize);
 		});
 
 	});
